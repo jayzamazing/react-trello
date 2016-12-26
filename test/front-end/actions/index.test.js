d377f222c453a2e6fa44a04acefe23198768f60e
jayzamazing <adrianjaylopez@gmail.com>
2016-12-25 23:01:11 -0500

    Finished create cardlist test in test/front-end/actions/index.test.js.

--- test/front-end/actions/index.test.js
+++ test/front-end/actions/index.test.js
@@ -23,77 +23,82 @@ describe('trello actions', () => {
         jsonObj['_id'] = ('1');
         //return response
         return [
-          200, {
-            body: jsonObj
-          }
+          200,
+          jsonObj
+
         ];
       })
       .patch('/boards/1')
       //send back reply to request
       .reply((uri, requestBody) => {
         //create json obj out of the request
-        var jsonObj = JSON.parse(requestBody);
+        var temp = JSON.parse(requestBody);
+        var jsonObj = temp.$push;
         //add _id field to json
         jsonObj['_id'] = ('1');
         //return response
         return [
-          200, {
-            body: jsonObj
-          }
+          200,
+          jsonObj
         ];
       })
       //request to create cardsList
       .post('/cardslists')
       //send back reply to request
       .reply((uri, requestBody) => {
-        console.log('hereeeee');
         //create json obj out of the request
         var jsonObj = JSON.parse(requestBody);
         //add _id field to json
-        jsonObj['_id'] = ('1');
+        jsonObj['_id'] = ('5');
         //return response
         return [
-          200, {
-            body: jsonObj
-          }
+          200,
+          jsonObj
         ];
       });
   });
   after(() => {
     nock.cleanAll();
   });
-    it('should create a board', () => {
-      //set up a mockstore
-      const store = mockStore({
-        boards: {}
-      });
-      //call createboard passing a title of the new board
-      return store.dispatch(actions.queries('boards', 'POST', {
-        title: 'blah'
-      }, 'create board'))
-        .then(() => {
-          //check response against expected values
-          var response = store.getActions()[0];
-          response.should.have.property('type');
-          response.type.should.equal('CREATE_BOARD_SUCCESS');
-          response.boards.boards.body.should.have.property('title');
-          response.boards.boards.body.title.should.equal('blah');
-          response.boards.boards.body.should.have.property('_id');
-          response.boards.boards.body._id.should.equal('1');
-        });
+  it('should create a board', () => {
+    //set up a mockstore
+    const store = mockStore({
+      boards: {}
     });
-    it('should create a cardslist', () => {
-      //set up a mockstore
-      const store = mockStore({
-        boards: {},
-        cardsList: {}
-      });
-      return store.dispatch(actions.queries('cardslists', 'POST', {title: 'fun'},
-      'create cardslist', 1))
+    //call createboard passing a title of the new board
+    return store.dispatch(actions.queries('boards', 'POST', {
+      title: 'blah'
+    }, 'create board'))
       .then(() => {
         //check response against expected values
         var response = store.getActions()[0];
-        console.log(response);
+        response.should.have.property('type');
+        response.type.should.equal('CREATE_BOARD_SUCCESS');
+        response.boards.boards.should.have.property('title');
+        response.boards.boards.title.should.equal('blah');
+        response.boards.boards.should.have.property('_id');
+        response.boards.boards._id.should.equal('1');
       });
-    });
   });
+  it('should create a cardslist', () => {
+    //set up a mockstore
+    const store = mockStore({
+      boards: {},
+      cardsList: {}
+    });
+    return store.dispatch(actions.queries('cardslists', 'POST', {
+      title: 'fun'
+    },
+        'create cardslist', 1))
+      .then(() => {
+        //check response against expected values
+        var response = store.getActions()[0];
+        response.should.have.property('type');
+        response.type.should.equal('CREATE_CARDLIST_SUCCESS');
+        response.boards.boards.should.have.property('cardsList');
+        response.boards.boards.cardsList.should.equal('5');
+        response.boards.boards.should.have.property('_id');
+        response.boards.boards._id.should.equal('1');
+      });
+  });
+});
