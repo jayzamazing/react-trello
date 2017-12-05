07cd724fbe9c505070bb2160610d42036fdd3889
jayzamazing <adrianjaylopez@gmail.com>
2017-12-04 23:21:27 -0500

    Changed return on login failure for strategy to error usable by redux forms. Returned the error inside auth. Changed return to redux form error for users.

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,13 @@ const {authenticated, authenticatedJWT} = require('../middlewares/authcheck');
 const Router = express.Router();
 Router.use(bodyParser.json());
 //deal with authentication and setting up session
-Router.post('/login', authenticated, (req, res) => {
-    const authToken = res.authToken;
-    res.json({authToken});
+Router.post('/login', authenticated, (err, req, res, next) => {
+  if (err) {
+    return res.status(err.code).json(err);
+  }
+}, (req, res) => {
+  const authToken = res.authToken;
+  res.json({authToken});
 });
 //deal with refreshing the token when it has expired
 Router.post('/refresh', authenticatedJWT, (req, res) => {
